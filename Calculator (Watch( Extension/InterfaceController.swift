529e3a916d377bbb40c4558b8e0d4dec059ccfaa
jaredrkozar <jared.rkozar@gmail.com>
2021-09-16 01:08:11 -0400

    initial watch app

---
+++ Calculator (Watch( Extension/InterfaceController.swift
@@ -0,0 +1,85 @@
+//
+//  InterfaceController.swift
+//  Calculator (Watch( Extension
+//
+//  Created by Jared Kozar on 9/16/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    @IBOutlet var equation: WKInterfaceLabel!
+    var equationText: String = ""
+    
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+        equationText = "Enter a number"
+        equation.setText(equationText)
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+    
+    @IBAction func zeroTapped() {
+        numberTapped(number: "0")
+    }
+    
+    @IBAction func oneTapped() {
+        numberTapped(number: "1")
+    }
+    
+    @IBAction func twoTapped() {
+        numberTapped(number: "2")
+    }
+    
+    @IBAction func threeTapped() {
+        numberTapped(number: "3")
+    }
+    
+    @IBAction func fourTapped() {
+        numberTapped(number: "4")
+    }
+    
+    @IBAction func fiveTapped() {
+         numberTapped(number: "5")
+    }
+    
+    @IBAction func sixTapped() {
+        numberTapped(number: "6")
+    }
+    
+    @IBAction func sevenTapped() {
+        numberTapped(number: "7")
+    }
+    
+    @IBAction func eightTapped() {
+        numberTapped(number: "8")
+    }
+    
+    @IBAction func nineTapped() {
+        numberTapped(number: "9")
+    }
+    
+    @IBAction func clearButtonTapped() {
+        equationText = "Enter a number"
+        equation.setText(equationText)
+    }
+    
+    func numberTapped(number: String) {
+        if equationText == "Enter a number" {
+            equationText = ""
+           equation.setText(equationText)
+        }
+        
+        equationText.append("\(number)")
+        
+       equation.setText(equationText)
+    }
+}
